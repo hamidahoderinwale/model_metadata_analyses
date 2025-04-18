cba1e31856762b63a846f3e4a28ee16c7f8166a8
Hamidah Oderinwale <75270160+hamidahoderinwale@users.noreply.github.com>
2025-04-18 16:22:42 -0700

    Update ds_folder.sh

--- scripts/ds_folder.sh
+++ scripts/ds_folder.sh
@@ -4,18 +4,22 @@ for file in *.csv *.json; do
   [[ -e "$file" ]] && mv -v "$file" datasets/
 done
 
-  base="${csv%.*}"
+cd datasets || exit 1
+
+  base="${csv%.*}" 
+  base="${json%.*}"
   base_name="${base##*/}"
   json="./${base_name}.json"
-
-  mkdir -p "$base_name"
+  csv="./${base_name}.csv"
+  
+mkdir -p "$base_name"
 
   if [[ -f "$json"]]; then
     mv -v"$json" "$base_name/"
-  else if 
+  elif
     if [[ -f "$csv"]]; then
     mv -v"$csv" "$base_name/"
-  else 
+  elif 
     echo "No matching JSON or CSVs, cannot be ran"
     mv -v "$csv" "$base_name/"
   fi
